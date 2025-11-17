f8674d5235339685dd3f5dadeea87b3186b87654
shishir c shetty <shishirshetty217@gmail.com>
2025-11-17 22:03:56 +0530

    fix: resolve React Hook conditional call in Input component

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -12,7 +12,8 @@ export interface InputProps
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, error, helperText, label, id, ...props }, ref) => {
-    const inputId = id || React.useId();
+    const generatedId = React.useId();
+    const inputId = id || generatedId;
 
     return (
       <div className="w-full space-y-2">
